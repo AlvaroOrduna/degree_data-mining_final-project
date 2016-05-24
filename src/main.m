7a5f29304a6bd6bc1f905d5230f625fb785f4f54
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2016-05-24 08:33:14 +0200

    main: Normalize data
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- src/main.m
+++ src/main.m
@@ -4,8 +4,13 @@ initScript = '';
 %% Load data
 fTrain = '../data/cleveland_tra.dat';
 fTest = '../data/cleveland_tst.dat';
-[numAtr, nClases, infoAtr, ejemplosTr, ejemplosTst, ejClase] = ...
-    lecturaDatos(fTrain, fTest);
+[numAtr, nClases, infoAtr, CE, CT, ejClase] = lecturaDatos(fTrain, fTest);
+
+%% Normalize data
+minimum = infoAtr(:, 1)';
+maximum = infoAtr(:, 2)';
+CE(:, 1:end - 1) = bsxfun(@rdivide, bsxfun(@minus, CE(:, 1:end - 1), minimum), (maximum - minimum));
+CT(:, 1:end - 1) = bsxfun(@rdivide, bsxfun(@minus, CT(:, 1:end - 1), minimum), (maximum - minimum));
 
 %% Run main script
 run(initScript);
