7ea15f5892019a48955da3d4822d9eef4219cd20
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2016-05-30 12:23:03 +0200

    knn_fuzzy: Clean main source
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- src/knn_fuzzy/knn_fuzzy_classify.m
+++ src/knn_fuzzy/knn_fuzzy_classify.m
@@ -17,30 +17,6 @@ function [ confusionMat, dataClasses ] = knn_fuzzy_classify( classifier, members
     confusionMat = zeros(size(classes, 1));
 
     for e = 1:size(distances, 1)
-%         % Weights vector for each class
-%         weights = zeros(size(classes));
-% 
-%         % Classes of k nearest neighbors
-%         kClasses = classifier(distancesOrder(e, 1:k), end);
-% 
-%         % Weights of k nearest neighbors
-%         kWeights = (1 ./ distances(e, distancesOrder(e, 1:k)) .^ 2);
-% 
-%         % For each neighbor, acumulate the weight
-%         for i = 1:k
-%             weights(kClasses(i)) = weights(kClasses(i)) + kWeights(i);
-%         end
-% 
-%         % Get the index of the heaviest class
-%         [~, i] = max(weights);
-% 
-%         % Get the heaviest class and add
-%         dataClasses(e) = classes(i);
-% 
-%         % Modify confusion matrix
-%         a = find(classes == data(e, end), 1);
-%         b = find(classes == dataClasses(e), 1);
-%         confusionMat(a, b) = confusionMat(a, b) + 1;
 
         % Pertenencias de los k más cercanos al ejemplo e
         kMembership = membership(distancesOrder(e, 1:k), :);
